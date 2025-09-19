0f6a3ce1ad948171346d22dbc8a08d44eb8dc155
Владимир Кононенко <Tyuiu.KononenkoVA@outlook.com>
2025-09-20 06:32:08 +0800

    Task1

---
+++ Tyuiu.KononenkoVA.Sprint1.Task1.V27/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.KononenkoVA.Sprint1.Task1.V27.Lib;
+
+namespace Tyuiu.KononekoVA.Sprint1.V22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Кононенко В. А. | ИСПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Выполнил: Кононенко Владимир Андреевич | ИСПб-25-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результаты по формуле (x+y)/2 и печатает его на экране        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x, y;
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(ds.Calculate(x,y));
+
+            Console.ReadLine();
+        }
+    }
+}
