37df4c23a0747430b07692ec6fb36f3b738e5c87
Владимир Кононенко <Tyuiu.KononenkoVA@outlook.com>
2025-09-21 19:52:45 +0800

    Task4

---
+++ Tyuiu.KononenkoVA.Sprint1.Task4.V30.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.KononenkoVA.Sprint1.Task4.V30.Lib;
+
+namespace Tyuiu.KononenkoVA.Sprint1.Task4.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 2;
+            double wait = 10;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KononenkoVA.Sprint1.Task4.V30/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.KononenkoVA.Sprint1.Task4.V30.Lib;
+
+namespace Tyuiu.KononenkoVA.Sprint1.Task4.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение X:");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Y:");
+            double y = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("(x + y^3)/(e^(2 - y)) = " + ds.Calculate(x,y));
+        }
+    }
+}   
