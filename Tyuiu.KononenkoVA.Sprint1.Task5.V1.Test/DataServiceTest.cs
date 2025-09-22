0e6cc542fe6f0b21df6d456196c28754be10adb4
Владимир Кононенко <Tyuiu.KononenkoVA@outlook.com>
2025-09-22 10:25:07 +0800

    Task5

---
+++ Tyuiu.KononenkoVA.Sprint1.Task5.V1.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.KononenkoVA.Sprint1.Task5.V1.Lib;
+
+namespace Tyuiu.KononenkoVA.Sprint1.Task5.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x1 = 15;
+            double y1 = 4;
+            double x2 = 8;
+            double y2 = 9;
+            double res = ds.DistanceBetweenDots(x1, y1, x2, y2);
+
+            int wait = 8;
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.KononenkoVA.Sprint1.Task5.V1/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.KononenkoVA.Sprint1.Task5.V1.Lib;
+
+namespace Tyuiu.KononenkoVA.Sprint1.Task5.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Ввведите X1:");
+            double x1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ввведите Y1:");
+            double y1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ввведите X2:");
+            double x2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ввведите Y2:");
+            double y2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            double res = ds.DistanceBetweenDots(x1, y1, x2, y2);
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
