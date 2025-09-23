fa718a206e7ad026a1d1cd5176f896c3ed0a170e
Владимир Кононенко <Tyuiu.KononenkoVA@outlook.com>
2025-09-23 14:53:26 +0800

    Task6V14

---
+++ Tyuiu.KononenkoVA.Sprint1.Task6.V14.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.KononenkoVA.Sprint1.Task6.V14.Lib;
+
+namespace Tyuiu.KononenkoVA.Sprint1.Task6.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string a = "Привет";
+            string b = "пока";
+            bool res1 = ds.CheckLowerCaseRusLetters(a);
+            bool res2 = ds.CheckLowerCaseRusLetters(b);
+            bool wait1 = false;
+            bool wait2 = true;
+            Assert.AreEqual(res1,wait1);
+            Assert.AreEqual(res2, wait2);
+
+        }
+    }
+}
---
+++ Tyuiu.KononenkoVA.Sprint1.Task6.V14/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.KononenkoVA.Sprint1.Task6.V14.Lib;
+
+namespace Tyuiu.KononenkoVA.Sprint1.Task6.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите строку только из строчных русских букв");
+            string str = Console.ReadLine();
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            bool res = ds.CheckLowerCaseRusLetters(str);
+
+            if (res == true)
+                Console.WriteLine("Cтрока составлена только из строчных русских букв. Красавчик!");
+            else
+                Console.WriteLine("Перечитывай условие! Cтрока составлена не только из строчных русских букв.");
+
+        }
+    }
+}
